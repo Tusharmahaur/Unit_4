2dd3e2c69806f5dcaa89e01dcf212cfc99b7cab8
Tusharmahaur <Tusharmahaur73@gmail.com>
2026-01-09 20:27:18 +0530

    this

--- getInfoMatrix.cpp
+++ getInfoMatrix.cpp
@@ -2,7 +2,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int m ,n;
+
+void getInfo(){
+    cout<<"Enter the row size and column size : ";
+    cin>>m>>n;
+}
+
+void Display(){
+    cout<<"your row is "<<m<<endl;
+    cout<<"Your column is "<<n<<endl;
+}
+
 int main(){
-    
+    getInfo();
+    Display();
     return 0;
 }
