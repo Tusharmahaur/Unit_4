3cb356ee56ffdf900e436b3d19c40a4c69dd54e9
Tusharmahaur <Tusharmahaur73@gmail.com>
2026-01-09 22:07:03 +0530

    new

---
+++ AreaFunctionOverloading.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Overloading{
+    public:
+        float h,r,s;
+
+void area(){
+    cout<<"Enter the height : ";
+    cin>>h;
+    cout<<"Enter the side : ";
+    cin>>s;
+
+    cout<<"Triangle area is : "<<(1/2.0)*h*s<<endl;
+}
+
+void area(int r){
+    
+    this ->r  = r;
+
+    cout<<"Circle Area is : "<<(22/7.0)*r*r<<endl; 
+}
+
+};
+int main(){
+    Overloading t1;
+    t1.area(5);
+        
+    return 0;
+}
